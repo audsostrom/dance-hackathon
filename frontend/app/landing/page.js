d4035486ec5b3f24ab0c0d6fcd161407fb631b33
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2024-05-26 16:37:23 -0700

    updated landing

--- frontend/app/landing/page.js
+++ frontend/app/landing/page.js
@@ -1,24 +1,20 @@
-"use client"
+'use client';
 import Link from 'next/link';
 import './landing.css';
 
 /**
  * @return {*} – Renders the Landing page
  */
-const WEBCAM_ENDPOINT = "http://127.0.0.1:5000/video_feed"
-const POSE_ENDPOINT = "http://127.0.0.1:5000/pose_feed"
-// console.log({POSE_ENDPOINT})
-
-
-
 
 export default async function Landing() {
-	return (
-      <div>
-         landing page
-         <Link href='/login'>
-            <button>go to login</button>
-         </Link>
+  return (
+    <div className='landing-container'>
+      <h1>DanceBrains!</h1>
+      <div className='hero-container'>
+        <Link className='login-btn' href='/login'>
+          Login to start!
+        </Link>
       </div>
-	);
+    </div>
+  );
 }
